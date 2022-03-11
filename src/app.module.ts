641e153f086cf4436dee3afdd58d77f90b621620
Felipe <fellipefreiire3@gmail.com>
2022-03-11 17:56:16 -0300

    add graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
+import { PrismaModule } from 'nestjs-prisma'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import config from './common/configs/config'
+import { GqlConfigService } from './gql-config.service'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+    PrismaModule.forRoot({
+      isGlobal: true,
+    }),
+
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      useClass: GqlConfigService,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
